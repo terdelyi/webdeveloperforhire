af44e545ffdcf018f345f6223bacc09fc0ded97b
Tamas Erdelyi <terdelyi@gmail.com>
2024-02-20 15:19:44 +0000

    Small graphical changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   theme: {
     colors: {
       "magenta": "#F6095E",
+      "complementer": "#F14583",
       "blue": "#095EF6",
       "dark-blue": "#043CB0",
       "pale-blue": "#556178",
@@ -22,13 +23,14 @@ module.exports = {
       "light-black": "#2B2A2A",
       "almost-black": "#403F3F",
       "green": "#93FF8A",
+      "mint-green": "#5296E6",
       "dark-green": "#01AE70",
       "light-green": "#EDFEEF",
 
     },
     fontFamily: {
-      text: ['Inter', 'sans-serif'],
-      title: ['Sora', 'serif'],
+      text: ['IBM Plex Sans', 'sans-serif'],
+      title: ['Rubik', 'sans-serif'],
     },
     extend: {
       lineHeight: {
